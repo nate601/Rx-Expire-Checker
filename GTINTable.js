21aade7a252ac3c3cb2503b0bd7746aa6ee941e8
Nathan Button <nathanebutton@gmail.com>
2021-05-07 23:38:04 -0500

    Move GS1 Table to component

---
+++ GTINTable.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Text, View} from 'react-native'
+import {TableView, InfoRow} from 'react-native-ios-kit';
+
+class GTINTable extends React.Component
+{
+    render(){
+        return (
+            <TableView header={"GS1"}>
+                <InfoRow
+                    title={"GTIN"}
+                    info={this.props.GTIN}
+                />
+                <InfoRow
+                    title={"Serial Number"}
+                    info={this.props.serial}
+                />
+                <InfoRow
+                    title={"Expiration Date"}
+                    info={this.props.expirationFormatted}
+                    theme={{barColor: this.props.isToBePulled ? "white" : "red"}}
+                />
+                <InfoRow
+                    title={"Lot Number"}
+                    info={this.props.lot}
+                />
+            </TableView>
+        );
+    }
+}
+export default GTINTable
+
